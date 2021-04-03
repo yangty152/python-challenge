7a56e9c58e2381c1be580dd95dfaff69bc7ac89d
yangty152 <35477773+yangty152@users.noreply.github.com>
2021-04-03 13:24:41 -0400

    Upload new main.py file

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,27 +1,34 @@
+# Modules
 import os
-
 import csv
 
+# Open budget_data.csv
 file = os.path.join('..','PyBank','Resources','budget_data.csv')
-print(file)
 with open(file) as csvfile:
     csvreader = csv.reader(csvfile, delimiter = ',')
+    csv_header = next(csvreader)
+    
+    #Declare and initiate variables
     Month = []
     ProfitLosses =[]
     totalProfitLosses = 0
     GreatestIncrease = 0
     GreatestDecrease = 0
-    csv_header = next(csvreader)
-    i = 0
     GreatestIncreaseMonth =""
     GreatestDecreaseMonth =""
     AverageChange = 0
     
+    #Loop through all rows in the file, and calcuate total Profit/Losses
+    #Assign values to the list of month and profitlosses
     for row in csvreader:
         totalProfitLosses += int(row[1])
         Month.append(row[0])
         ProfitLosses.append(row[1])
-
+    
+    #Calculate averate changes - change between end period profit/losses and beginning profit/losses, and then divide by total periods
+    AverageChange = (int(ProfitLosses[len(ProfitLosses)-1])-int(ProfitLosses[0]))/len(ProfitLosses)
+    AverageChange = "{:.2f}".format(AverageChange)
+    #Compare and find the greatest increase and greatest decrease value
     for i in range(len(Month)):
         if int(ProfitLosses[i]) > GreatestIncrease:
             GreatestIncrease = int(ProfitLosses[i])
@@ -35,7 +42,7 @@ with open(file) as csvfile:
             GreatestDecrease = GreatestDecrease
 
         i+=1
-    AverageChange = totalProfitLosses/len(Month)
+    #Print out the results
     print("Financial Analysis")
     print("-----------------------------------------------------")
     print(f"Total Months: {len(Month)}")
@@ -44,4 +51,17 @@ with open(file) as csvfile:
     print(f"Greatest Increase in Profits: {GreatestIncreaseMonth} (${GreatestIncrease})")
     print(f"Greatest Decrease in Profits: {GreatestDecreaseMonth} (${GreatestDecrease})")
 
+# Write output to a text file
+output_path = os.path.join("..", "PyBank","output", "PyBankResult.txt")
+
+with open(output_path, 'w', newline='') as outputfile:
+
+    outputfile.write(f"Financial Analysis"'\n')
+    outputfile.write(f"-----------------------------------------------------"'\n')
+    outputfile.write(f"Total Months: {len(Month)}"'\n')
+    outputfile.write(f"Total: ${totalProfitLosses}"'\n')
+    outputfile.write(f"Average Change: ${AverageChange}"'\n')
+    outputfile.write(f"Greatest Increase in Profits: {GreatestIncreaseMonth} (${GreatestIncrease})"'\n')
+    outputfile.write(f"Greatest Decrease in Profits: {GreatestDecreaseMonth} (${GreatestDecrease})"'\n')
+    outputfile.close
 
